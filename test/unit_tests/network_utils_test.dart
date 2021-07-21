9e601470f1f35145881ec2d65c2e0273fed4b6e8
Izzan Fakhril Islam <izzanfi@hotmail.com>
2021-07-22 02:55:45 +0700

    init unit tests

---
+++ test/unit_tests/network_utils_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:covidgraph/utils/network.dart';
+
+void main() {
+  test("NetworkInterface called and exists", () {
+    final networkInterfaceObj = NetworkInterface();
+  });
+}
